c26fd89819c887680f479561bafbd90169e9a331
zksytmkn <zksytmkn4869@gmail.com>
2023-08-16 00:41:19 +0900

    wip:Prisma Service(#8)

--- src/main.ts
+++ src/main.ts
@@ -10,11 +10,9 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.enableCors({
     credentials: true,
-    origin: [
-      'http://localhost:3000',
-      'https://frontend-todo-nextjs.vercel.app',
-    ],
+    origin: ['http://localhost:3000'],
   });
+  app.use(cookieParser());
   await app.listen(3005);
 }
 bootstrap();
